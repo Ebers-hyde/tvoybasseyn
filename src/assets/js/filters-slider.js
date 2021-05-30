8e8bb3c984c3f97ebfe9b46a7a469f0aaec7e5fc
tupical <tupical@gmail.com>
2021-05-30 17:37:07 +0300

    artem office

--- src/assets/js/filters-slider.js
+++ src/assets/js/filters-slider.js
@@ -1,10 +1,13 @@
 if (document.querySelector('.pool-filters__slider')) {
 	var swiper = new Swiper('.pool-filters__slider .swiper-container', {
-		slidesPerView: 2,
+		slidesPerView: 'auto',
+		initialSlide: $('.pool-filters__slider .swiper-slide').length,
 		speed: 1000,
 		loop: false,
 		autoHeight: true,
 		spaceBetween: 5,
 		grabCursor: true,
 	});
+
+	setTimeout(() => swiper.slideTo(0, 500, false), 1000);
 }
