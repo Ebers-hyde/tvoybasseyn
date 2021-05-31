3a64aa7e4930d329c6612d4feaa9e2945b06a29c
tupical <tupical@gmail.com>
2021-05-31 17:53:21 +0300

    anton office

--- src/assets/js/product.js
+++ src/assets/js/product.js
@@ -1,3 +1,10 @@
+if(document.querySelector('.product')) {
+    document.querySelector('.open_modelsDropdown').onclick = function(evt) {
+        evt.preventDefault();
+        document.querySelector('.product__modelDropdowns').classList.toggle('opened');
+    }
+}
+
 if (document.querySelector('.product__power-list')) {
     let produc = document.querySelectorAll('.product__power-list .product__power-item');
     for (var i = 0; i < produc.length; i++) {
